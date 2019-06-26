2da4cc1f0a10738ccaf0d7da00f5be8f99e35a47
fenikkusuro <ingsistemas7.pj@gmail.com>
2019-06-25 23:38:07 -0400

    update send.php

--- send.php
+++ send.php
@@ -5,13 +5,13 @@ isset($_POST['asunto']) && !empty($_POST['asunto']) &&
 isset($_POST['mensaje']) && !empty($_POST['mensaje']))
    
 {
-    &destino = "ingsistemas7.pj@gmail.com";
+    $destino = "ingsistemas7.pj@gmail.com";
     $desde   = "From:". "Furia Koryo";
     $nombre  = $_POST['nombre'];
     $email   = $_POST['email'];
     $asunto  = $_POST['asunto'];
     $mensaje = $_POST['mensaje'];
-    mail(&destino,$desde,$nombre,$email,$asunto,$mensaje)
+    mail($destino,$desde,$nombre,$email,$asunto,$mensaje)
     echo "Correo enviado...";
 }else{
     echo "Problemas al enviar...";
