1d290de221b08f3048c7954cc46f150c3f4f8ae4
fenikkusuro <ingsistemas7.pj@gmail.com>
2019-06-25 23:30:41 -0400

    create send.php
    1

---
+++ send.php
@@ -0,0 +1,22 @@
+<?php 
+if (isset($_POST['nombre']) && !empty($_POST['nombre']) &&
+isset($_POST['email']) && !empty($_POST['email']) &&
+isset($_POST['asunto']) && !empty($_POST['asunto']) &&
+isset($_POST['mensaje']) && !empty($_POST['mensaje']))
+   
+{
+    &destino = "ingsistemas7.pj@gmail.com";
+    $desde   = "From:". "Furia Koryo";
+    $nombre  = $_POST['nombre'];
+    $email   = $_POST['email'];
+    $asunto  = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    mail(&destino,$desde,$nombre,$email,$asunto,$mensaje)
+    echo "Correo enviado...";
+}else{
+    echo "Problemas al enviar...";
+}
+
+
+
+?> 
